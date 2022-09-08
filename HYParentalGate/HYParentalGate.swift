ce197ec393a5cf734327e48d5369bd98e26e5b7a
Magy Elias <46000231+Magy-Elias@users.noreply.github.com>
2022-09-08 19:39:48 +0200

    Update HYParentalGate.swift
    - Adjust the init window for ios 13.0,*

--- HYParentalGate/HYParentalGate.swift
+++ HYParentalGate/HYParentalGate.swift
@@ -89,8 +89,17 @@ public class HYParentalGate: NSObject, HYParentalGateViewDelegate, UIGestureReco
     }
     
     fileprivate func initWindow() {
-        let appDelegate = UIApplication.shared.delegate
-        window = appDelegate!.window!
+        if #available(iOS 13.0, *) {
+            window = UIApplication.shared.connectedScenes
+            .filter({$0.activationState == .foregroundActive})
+            .map({$0 as? UIWindowScene})
+            .compactMap({$0})
+            .first?.windows
+            .filter({$0.isKeyWindow}).first!
+        } else {
+            let appDelegate = UIApplication.shared.delegate
+            window = appDelegate!.window!
+        }
     }
 
     fileprivate func addOverlay() {
